b24ea8fe32d43969ad80ec4486e23ef8a9704d6a
cla+ana <50658467+cladallaqua@users.noreply.github.com>
2019-05-15 20:46:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 provider "scaleway" {
+  SCALEWAY_ORGANIZATION = "${var.SCALEWAY_ORGANIZATION}"
+  SCALEWAY_TOKEN        = "${var.SCALEWAY_TOKEN}"
   region = "${var.region}"
-  organization = "${var.SCALEWAY_ORGANIZATION}"
-  token        = "${var.SCALEWAY_TOKEN}"
 }
 
+
+resource "scaleway_server" "terrclarisseana" {
+  name  = "terrclarisseana"
+  image = "27452e61-310e-4fe5-93af-0a0bdf4c20a6"
+  type  = "START1-S"
+}
+
+resource "scaleway_ip" "ip" {
+  server = "${scaleway_server.terrclarisseana.id}"
+}
