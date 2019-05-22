d0d9d4a0e54a74f30adb20fc3985574b1e1e85ae
cla+ana <50658467+cladallaqua@users.noreply.github.com>
2019-05-22 12:14:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,54 @@
-
 provider "scaleway" {
-  region = "${var.region}"
   organization = "${var.org}"
-  token = "${var.tok}"
+  token        = "${var.tok}"
+  region       = "par1"
 }
 
-data "scaleway_image" "terraformclarisse" {
+resource "scaleway_ip" "ip" {
+  server = "${scaleway_server.cloud-courses.id}"
+}
+
+data "scaleway_image" "cloud-courses" {
   architecture = "x86_64"
   name         = "Ubuntu Bionic"
 }
 
-resource "scaleway_server" "terraformclarisse" {
-  name  = "terraformclarisse"
-  image = "${data.scaleway_image.terraformclarisse.id}"
+resource "scaleway_server" "cloud-courses" {
+  name  = "cloud-courses"
+  public_ip = "51.158.187.107"
+  image = "${data.scaleway_image.cloud-courses.id}"
   type  = "START1-S"
-}
-resource "scaleway_ip" "ip" {
-  server = "${scaleway_server.terraformclarisse.id}"
+
 }
 
-
-resource "scaleway_volume" "terraformclarisse" {
-  name       = "terraformclarissevolume"
+resource "scaleway_volume" "cloud-courses" {
+  name       = "cloud-coursesvolume"
   size_in_gb = 50
   type       = "l_ssd"
 }
 
+
+resource "scaleway_security_group" "http" {
+  name        = "http"
+  description = "allow HTTP and HTTPS traffic"
+}
+
+resource "scaleway_security_group_rule" "http_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 80
+}
+
+resource "scaleway_security_group_rule" "https_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 443
+}
